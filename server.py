4916f92cbfdb8897b4cece8e813783018b466189
root <root@Apline-3.23.3-Tornado-Python>
2026-02-12 08:17:01 +0000

    server.py and requirements.txt for tornado tutorial

---
+++ server.py
@@ -0,0 +1,22 @@
+import asyncio
+import tornado
+import cpuinfo, json
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        message=cpuinfo.get_cpu_info()
+        self.write(json.dumps(message))
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+    ])
+
+async def main():
+    app = make_app()
+    print("http://0.0.0.0:8888")
+    app.listen(8888)
+    await asyncio.Event().wait()
+
+if __name__ == "__main__":
+    asyncio.run(main())
